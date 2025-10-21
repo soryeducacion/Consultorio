366d790b189f0e8887579ff837c4777a947e179b
slopez707 <educacion.sory@gmail.com>
2025-10-21 08:28:16 -0600

    metodos

--- src/principal/Paciente.java
+++ src/principal/Paciente.java
@@ -1,13 +1,21 @@
 package principal;
+
+import java.util.ArrayList;
+
+
 //herencia palabra reservada extends
 public class Paciente extends Persona {
 	private String fecha_nacimiento;
 	private char sexo;
 	private String direccion;
 	
+	
+	ArrayList<Persona> lista_paciente = new ArrayList<>();
+	
 	public Paciente() {}
 	
-	public Paciente(String fecha_nac, char sexo, String direccion) {
+	public Paciente(String fecha_nac, char sexo, String direccion, String nombre, String apellido, String correo, long telefono) {
+		super(nombre,apellido,correo,telefono);
 		this.fecha_nacimiento = fecha_nac;
 		this.sexo = sexo;
 		this.direccion = direccion;
@@ -37,7 +45,47 @@ public class Paciente extends Persona {
 		this.direccion = direccion;
 	}
 	
+	@Override
+	public String getAtributos() {
+		return super.getAtributos() +
+				"Direccion: " + this.direccion +
+			   "\n Sexo: " + this.sexo +
+			   "\n Fecha Nacimiento: " + this.fecha_nacimiento;				
+	}
 	
 	
+	public int esMayorEdad() {
+		//	crear un código para validar si el paciente es mayor de edad y este pueda firmar su propio consentimiento médico
+	
+		return 0;
+	} 
+	
+	
+	public int calcularEdad() {
+		//en este método calcula la edad que tiene actualmente el paciente 
+		return 0;
+	}
+	
+	public String generarIdentificadorUnico() {
+		/*Crea un código único basado en nombre + fecha (ej. “LG20251016”).*/
+		return "";
+	}
+	
+	public void altaPaciente(Persona paciente) {
+		// alta de pacientes en el arreglo lista_paciente
+		lista_paciente.add(paciente);
+	}
+	
+
+    public void mostrarPacientes() {
+    	System.out.println("Listado de Pacientes");
+    	for (Persona lista : lista_paciente){
+    		System.out.println(lista.getAtributos());
+    	}
+    }
+    
+    public void eliminarPaciente() {
+    	
+    }
 
 }
--- src/principal/Persona.java
+++ src/principal/Persona.java
@@ -1,5 +1,7 @@
 package principal;
 
+import java.util.Scanner;
+
 public class Persona {
 	//MODIFICADORES DE ACCESO private, protected, public
 	//ENCAPSULAMIENTO
@@ -18,14 +20,124 @@ public class Persona {
 		this.telefono = tel;
 	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public long getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(long telefono) {
+		this.telefono = telefono;
+	}
+	
+	public String getAtributos() {
+		return "Nombre: " + this.nombre +
+			   "\n Apellido: " + this.apellido +
+			   "\n Correo: " + this.correo +
+			   "\n Telefono: " + this.telefono;
+				
+	}
+	
+
+	
+	public boolean verificarTelefono(String telefono) {
+		/*esta función deberás terminarla y la lógica de negocio es la siguiente 
+		 * debe tener 10 digitos el campo de telefono.
+		 * que no este vacio*/
+		
+		boolean verificar = true;
+		String mensaje = "";
+		if(telefono.length() >= 10) {
+			mensaje = mensaje + "\n Tiene más de 10 digitos: " + telefono.length();
+			verificar = false;
+		}else if(telefono.length() <= 10) {
+			mensaje = mensaje + "\n Tiene menos digitos: " + telefono.length();
+			verificar = false;
+		}
+		
+		if(telefono.isEmpty()) {
+			mensaje = mensaje + "\n deberá estar capturado: " + telefono.length();
+			verificar = false;
+		}
+		
+		
+		if(verificar == false) {
+			
+			System.out.println("El numero de telefono tiene los siguientes errores: \n" + mensaje);
+		}
+		
+		return verificar;
+	}
+	
+	public String validarCorreo() { 
+		//crea un algoritmo para validar si es un correo valido
+		return "";
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Persona persona = new Persona("Soraya", "Lopez","sory@h.co",3333333);
-		
-        System.out.println("Nombre " + persona.nombre + " Apellido "+ persona.apellido);
+		       
+        Paciente paciente = new Paciente();
+        
+        System.out.println(persona.verificarTelefono("77777777777777777")); //aqui se verifica el método 
        
-        Paciente paciente = new Paciente("199401-01-01",'F',"Salvador 44");
-        System.out.println("Fecha " +paciente.getFecha_nacimiento());
+        //System.out.println(paciente.getAtributos());
+        int op = 0;
+        boolean entrar = false;
+        Scanner sc = new Scanner(System.in);
+        
+        while(entrar) {
+        	//crea el menu en base a los casos que tiene switch
+        	System.out.println("1. Altas");
+        
+        
+	        switch (op) {
+				case 1: // alta paciente
+					paciente.altaPaciente(new Paciente("199401-01-01",'F',"Salvador 44","Ela","Gonzalez","fff@k.com",55444444));
+					
+					break;
+				case 2: //actualizar info de paciente
+					
+					break;
+					
+				case 3: //eliminar paciente de la lista_paciente
+					//investiga como se elimina un registro de un ArrayList de tipo Clase en java
+					
+					break;
+				case 4: //mostrar lista de pacientes
+					paciente.mostrarPacientes();
+					break;
+				case 5: // salir del menu
+				
+				break;
+			default:
+				break;
+			}
+	        
+        }
         
 	}
 
